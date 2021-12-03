636359fd27e1501620fe6ec99db083d78e230d7a
Thando <noluthando.mhlontlo11@gmail.com>
2021-12-03 13:35:07 +0200

    Commit all

--- About.aspx.cs
+++ About.aspx.cs
@@ -16,7 +16,7 @@ namespace TechnicalCV
 
         protected void LinkButton1_Click(object sender, EventArgs e)
         {
-            Response.Redirect("HomePage.aspx");
+           Response.Redirect("HomePage.aspx");
         }
     }
 }
